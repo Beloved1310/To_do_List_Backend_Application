d6e396ad9bf6f69900c7ce58a26ee7d3f37700e6
Beloved1310 <adeoluwafisayomi@gmail.com>
2023-01-29 13:18:48 -0800

    added environment variables

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //get the function name from the db.js file which is exported
-const connectToMongo = require("./db");
+
 //run the function call
 
 //install the express and load the nodejs application
 const express = require("express");
 const app = express();
 const {PORT} = require('./config');
+const connectToMongo = require("./db");
 var cors = require('cors')
 
 app.use(cors({ origin: "*" }));
